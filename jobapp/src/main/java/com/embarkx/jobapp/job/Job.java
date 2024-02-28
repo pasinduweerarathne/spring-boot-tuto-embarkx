6dcfd04a3dc1ebd8576183ede88e55b45123b724
pasinduweerarathne <pasiweerarathne98@gmail.com>
2024-02-29 00:32:43 +0530

    add job and job controller

---
+++ jobapp/src/main/java/com/embarkx/jobapp/job/Job.java
@@ -0,0 +1,67 @@
+package com.embarkx.jobapp.job;
+
+public class Job {
+    private long id;
+    private long title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    public Job(long id, long title, String description, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getTitle() {
+        return title;
+    }
+
+    public void setTitle(long title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
